1f7ef112b05b3566af0cf65c4f646f9ea1b15a59
Jared Lewis <jared@jared.kiwi.nz>
2025-01-08 16:42:30 +1100

    feat: Fetch data from https://github.com/CMIP-REF/ref-sample-data

---
+++ packages/ref/src/ref/testing.py
@@ -0,0 +1,52 @@
+import importlib.resources
+import os
+import shutil
+from pathlib import Path
+
+import pooch
+from loguru import logger
+
+
+def _determine_test_directory() -> Path | None:
+    expected = Path(__file__).parents[4] / "tests" / "test-data"
+
+    if not expected.exists():
+        return None
+    return expected
+
+
+def _build_sample_data_registry(sample_data_version: str) -> pooch.Pooch:
+    registry = pooch.create(
+        path=pooch.os_cache("ref_sample_data"),
+        base_url="https://raw.githubusercontent.com/CMIP-REF/ref-sample-data/refs/tags/{version}/data/",
+        version=sample_data_version,
+        env="REF_SAMPLE_DATA_DIR",
+    )
+
+    with importlib.resources.files("ref").joinpath("datasets").joinpath("sample_data.txt").open("rb") as fh:
+        registry.load_registry(fh)
+
+    return registry
+
+
+TEST_DATA_DIR = _determine_test_directory()
+SAMPLE_DATA_VERSION = "v0.1.1"
+
+
+def fetch_sample_data(version: str = SAMPLE_DATA_VERSION) -> None:
+    sample_registry = _build_sample_data_registry(version)
+
+    output_dir = TEST_DATA_DIR / "sample-data"
+
+    if output_dir.exists():
+        logger.warning("Removing existing sample data")
+        shutil.rmtree(output_dir)
+
+    for key in sample_registry.registry.keys():
+        fetch_file = sample_registry.fetch(key)
+
+        if TEST_DATA_DIR:
+            linked_file = output_dir / key
+            linked_file.parent.mkdir(parents=True, exist_ok=True)
+            logger.info(f"Creating symlink for {linked_file}")
+            os.symlink(fetch_file, linked_file)
---
+++ tests/test-data/README.md
@@ -0,0 +1,15 @@
+# Test data
+
+## Sample Data
+
+A set of sample data that can be used to test the REF.
+These data are used in the REF tests and are not intended to be used for any other purpose
+as they are downsampled to reduce the size of the test dataset.
+
+These data can be fetched using `make fetch-test-data`.
+This will remove any exisiting sample data.
+
+
+## CMEC Output
+
+Reference output from the CMEC model is stored in the `cmec-output` directory.

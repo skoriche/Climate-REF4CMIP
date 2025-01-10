324a23b827cd90def93e6f0ccdb95c8a7af068f1
Jared Lewis <jared@jared.kiwi.nz>
2025-01-10 14:59:56 +1100

    chore: Update stubs for pooch

--- packages/ref/src/ref/testing.py
+++ packages/ref/src/ref/testing.py
@@ -51,16 +51,18 @@ def fetch_sample_data(version: str = SAMPLE_DATA_VERSION) -> None:
     """
     sample_registry = _build_sample_data_registry(version)
 
-    output_dir = TEST_DATA_DIR / "sample-data"
+    output_dir = None
 
-    if output_dir.exists():
-        logger.warning("Removing existing sample data")
-        shutil.rmtree(output_dir)
+    if TEST_DATA_DIR:
+        output_dir = TEST_DATA_DIR / "sample-data"
+        if output_dir.exists():
+            logger.warning("Removing existing sample data")
+            shutil.rmtree(output_dir)
 
     for key in sample_registry.registry.keys():
         fetch_file = sample_registry.fetch(key)
 
-        if TEST_DATA_DIR:
+        if output_dir:
             linked_file = output_dir / key
             linked_file.parent.mkdir(parents=True, exist_ok=True)
             logger.info(f"Creating symlink for {linked_file}")

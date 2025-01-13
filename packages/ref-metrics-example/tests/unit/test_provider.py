ca5ce026ff78c9a86dcea66777daa86314ee99a0
Jared Lewis <jared@jared.kiwi.nz>
2025-01-13 14:20:22 +1100

    test: Remove unused tests

--- packages/ref-metrics-example/tests/unit/test_provider.py
+++ packages/ref-metrics-example/tests/unit/test_provider.py
@@ -1,11 +1,6 @@
 from cmip_ref_metrics_example import __version__, provider
 
 
-# Placeholder to get CI working
-def test_version():
-    assert __version__ == "0.1.0"
-
-
 def test_provider():
     assert provider.name == "Example"
     assert provider.slug == "example"

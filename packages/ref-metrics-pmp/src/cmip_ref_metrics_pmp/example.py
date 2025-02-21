2ac9143904474df175d63627d78858704bc68b36
Jiwoo Lee <lee1043@llnl.gov>
2025-02-21 08:33:41 -0800

    in progress

--- packages/ref-metrics-pmp/src/cmip_ref_metrics_pmp/example.py
+++ packages/ref-metrics-pmp/src/cmip_ref_metrics_pmp/example.py
@@ -1,3 +1,4 @@
+import subprocess
 from pathlib import Path
 from typing import Any
 
@@ -74,21 +75,23 @@ def format_cmec_output_bundle(dataset: xr.Dataset) -> dict[str, Any]:
     return cmec_output
 
 
-class AnnualCycle(Metric):
+class ExtratropicalModesOfVariability_PDO(Metric):
     """
     Calculate the annual cycle for a dataset
     """
 
-    name = "PMP Annual Cycle"
-    slug = "pmp-annual-cycle"
+    name = "PMP Extratropical modes of variability PDO"
+    slug = "pmp-extratropical-modes-of-variability-pdo"
 
     data_requirements = (
         DataRequirement(
             source_type=SourceDatasetType.CMIP6,
             filters=(
-                FacetFilter(facets={"frequency": "mon", "experiment_id": ("historical", "amip")}),
+                FacetFilter(
+                    facets={"frequency": "mon", "experiment_id": ("historical"), "variable_id": "ts"}
+                ),
                 # Ignore some experiments because they are not relevant
-                FacetFilter(facets={"experiment_id": ("hist-*")}, keep=False),
+                FacetFilter(facets={"experiment_id": ("amip")}, keep=False),
             ),
             # Add cell areas to the groups
             # constraints=(AddCellAreas(),),
@@ -117,8 +120,59 @@ class AnnualCycle(Metric):
 
         input_datasets = definition.metric_dataset[SourceDatasetType.CMIP6]
 
-        annual_mean_global_mean_timeseries = calculate_annual_cycle(input_files=input_datasets.path.to_list())
+        # NEED HELP: Retrive info
+        # QUESTION: reference data?
+        source_id = input_datasets["source_id"].unique()[0]
+        member_id = input_datasets["member_id"].unique()[0]
+        path_to_model_nc_file = input_datasets.path.to_list()  # this is going to be list of strings
+        path_to_ref_nc_file = load_ref_data()
+        reference_data_name = load_ref_data_name()
+        output_directory_path = definition.output_directory
+
+        cmd = [
+            "variability_modes.py",
+            "-p",
+            "param/pmp_param_MoV-PDO.py",
+            "--modnames",
+            source_id,
+            "--realization",
+            member_id,
+            "--modpath",
+            *path_to_model_nc_file,
+            "--reference_data_path",
+            path_to_ref_nc_file,
+            "--reference_data_name",
+            reference_data_name,
+            "--results_dir",
+            output_directory_path,
+        ]
+
+        # Run the command and capture the output
+        proc = subprocess.run(
+            cmd,
+            capture_output=True,
+            text=True,
+            check=False,
+        )
+
+        # Print the command output
+        print("Output:\n", proc.stdout)
+
+        # Print any errors
+        if proc.stderr:
+            print("Error:\n", proc.stderr)
+
+        #annual_mean_global_mean_timeseries = calculate_annual_cycle(input_files=input_datasets.path.to_list())
+        
+        # Expected outcome from the run: a JSON file and some PNG files
+        # QUESTION: Do we expect the metric to return the output, or save it somewhere should be fine?
+
+        # Load json as dict and return it
+        # About png files --- talk to Min
+        
+        result_dict = SOMETHING()
 
         return MetricResult.build_from_output_bundle(
-            definition, format_cmec_output_bundle(annual_mean_global_mean_timeseries)
+            #definition, format_cmec_output_bundle(annual_mean_global_mean_timeseries)
+            definition, result_dict
         )

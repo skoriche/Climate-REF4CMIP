65cbf311a20a15cc348172bb6a7840b67a9f10b0
Bouwe Andela <b.andela@esciencecenter.nl>
2025-03-07 14:11:48 +0100

    Nicer cli

--- packages/ref/src/cmip_ref/cli/providers.py
+++ packages/ref/src/cmip_ref/cli/providers.py
@@ -2,13 +2,17 @@
 Manage the REF providers.
 """
 
+import pandas as pd
 import typer
 from loguru import logger
+from rich.console import Console
 
+from cmip_ref.cli._utils import pretty_print_df
 from cmip_ref.provider_registry import ProviderRegistry
-from cmip_ref_core.providers import CondaMetricsProvider
+from cmip_ref_core.providers import CondaMetricsProvider, MetricsProvider
 
 app = typer.Typer(help=__doc__)
+console = Console()
 
 
 @app.command(name="list")
@@ -20,8 +24,25 @@ def list_(ctx: typer.Context) -> None:
     db = ctx.obj.database
     with db.session.begin():
         provider_registry = ProviderRegistry.build_from_config(config, db)
-    providers = "\n".join([f"{p.name}: {p.version}" for p in provider_registry.providers])
-    print(providers)
+
+    def get_env(provider: MetricsProvider) -> str:
+        if isinstance(provider, CondaMetricsProvider):
+            if provider.env_path.exists():
+                return f"{provider.env_path}"
+            return "not installed"
+        return ""
+
+    results_df = pd.DataFrame(
+        [
+            {
+                "provider": provider.slug,
+                "version": provider.version,
+                "conda environment": get_env(provider),
+            }
+            for provider in provider_registry.providers
+        ]
+    )
+    pretty_print_df(results_df, console=console)
 
 
 @app.command()
@@ -35,18 +56,20 @@ def create_env(ctx: typer.Context, provider: str | None = None) -> None:
         providers = ProviderRegistry.build_from_config(config, db).providers
 
     if provider is not None:
-        available = ", ".join([f'"{p.name}"' for p in providers])
-        providers = [p for p in providers if p.name == provider]
+        available = ", ".join([f'"{p.slug}"' for p in providers])
+        providers = [p for p in providers if p.slug == provider]
         if not providers:
             msg = f'Provider "{provider}" not available. Choose from: {available}'
             logger.error(msg)
             raise typer.Exit(code=1)
 
     for provider_ in providers:
+        txt = f"virtual environment for provider {provider_.slug}"
         if isinstance(provider_, CondaMetricsProvider):
-            msg = f"Creating virtual environment for provider {provider_.name} " f"in {provider_.env_path}"
-            logger.info(msg)
+            logger.info(f"Creating {txt} in {provider_.env_path}")
             provider_.create_env()
-            logger.info(f"Finished creating virtual environment for {provider_.name}")
+            logger.info(f"Finished creating {txt}")
         else:
-            logger.info(f"Skipping {provider_.name} because it does use virtual environments.")
+            logger.info(f"Skipping creating {txt} because it does use virtual environments.")
+
+    list_(ctx)

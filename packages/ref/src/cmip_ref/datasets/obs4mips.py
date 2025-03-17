c57f8546270a6f906b415268e07a0ebd9695ec28
Dora Hegedus <78150129+dhegedus99@users.noreply.github.com>
2025-03-17 13:45:31 +0000

    Update packages/ref/src/cmip_ref/datasets/obs4mips.py
    Co-authored-by: Jared Lewis <jared@jared.kiwi.nz>

--- packages/ref/src/cmip_ref/datasets/obs4mips.py
+++ packages/ref/src/cmip_ref/datasets/obs4mips.py
@@ -109,10 +109,7 @@ def parse_obs4mips(file: str) -> dict[str, Any | None]:
         )
         return info
 
-    except TypeError:
-        logger.warning(traceback_message)
-        return {"INVALID_ASSET": file, "TRACEBACK": traceback_message}
-    except AttributeError:
+    except TypeError, AttributeError:
         logger.warning(traceback_message)
         return {"INVALID_ASSET": file, "TRACEBACK": traceback_message}
     except Exception:

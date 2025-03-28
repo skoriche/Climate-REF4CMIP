668144275c1c162df7ff9207169538b7b2493d1b
Jared Lewis <jared@jared.kiwi.nz>
2025-03-28 11:37:53 -0600

    chore: Fix incorrect import

--- tests/integration/test_ar7_ft.py
+++ tests/integration/test_ar7_ft.py
@@ -5,7 +5,7 @@ import pytest
 
 from cmip_ref.config import default_metric_providers
 from cmip_ref.database import Database
-from cmip_ref.models import MetricExecution
+from cmip_ref.models import MetricExecutionGroup
 
 
 def create_execution_dataframe(executions):
@@ -67,8 +67,8 @@ def test_solve_ar7_ft(
     # This will also create conda environments for the metric providers
     invoke_cli(["--verbose", "solve", "--timeout", f"{60 * 60}"])
 
-    executions = db.session.query(MetricExecution).all()
-    df = create_execution_dataframe(executions)
+    execution_groups = db.session.query(MetricExecutionGroup).all()
+    df = create_execution_dataframe(execution_groups)
     print(df)
 
     # Check that all 3 metric providers have been used

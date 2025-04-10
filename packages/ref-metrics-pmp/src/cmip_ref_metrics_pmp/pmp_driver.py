213ae1769305f052dfa5ed03746128717abea27a
Jiwoo Lee <lee1043@llnl.gov>
2025-04-10 11:54:33 -0700

    resolve conflict

--- packages/ref-metrics-pmp/src/cmip_ref_metrics_pmp/pmp_driver.py
+++ packages/ref-metrics-pmp/src/cmip_ref_metrics_pmp/pmp_driver.py
@@ -178,9 +178,8 @@ def build_pmp_command(
             else:
                 cmd.extend([f"--{key}"])
 
-    print("-- PMP command to run --")
-    print("[PMP] Command to run:", " ".join(map(str, cmd)))
-    print("[PMP] Command generation for the driver completed.")
-    logger.info(f"pmp command to run: {cmd}")
+    logger.info("-- PMP command to run --")
+    logger.info("[PMP] Command to run:", " ".join(map(str, cmd)))
+    logger.info("[PMP] Command generation for the driver completed.")
 
     return cmd

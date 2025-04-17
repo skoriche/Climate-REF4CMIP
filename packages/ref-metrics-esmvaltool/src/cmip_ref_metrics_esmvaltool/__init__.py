6c40387c58aba5e887be58a0738b4175d46babf0
Bouwe Andela <b.andela@esciencecenter.nl>
2025-04-17 14:38:37 +0200

    Add observational data

--- packages/ref-metrics-esmvaltool/src/cmip_ref_metrics_esmvaltool/__init__.py
+++ packages/ref-metrics-esmvaltool/src/cmip_ref_metrics_esmvaltool/__init__.py
@@ -3,6 +3,7 @@ Rapid evaluating CMIP data with ESMValTool.
 """
 
 import cmip_ref_metrics_esmvaltool.metrics
+from cmip_ref_core.dataset_registry import dataset_registry_manager
 from cmip_ref_core.providers import CondaMetricsProvider
 from cmip_ref_metrics_esmvaltool._version import __version__
 from cmip_ref_metrics_esmvaltool.recipe import _ESMVALTOOL_COMMIT
@@ -19,3 +20,11 @@ provider = CondaMetricsProvider(
 for _metric_cls_name in cmip_ref_metrics_esmvaltool.metrics.__all__:
     _metric_cls = getattr(cmip_ref_metrics_esmvaltool.metrics, _metric_cls_name)
     provider.register(_metric_cls())
+
+# Register OBS, OBS6, and raw data
+dataset_registry_manager.register(
+    "esmvaltool",
+    "https://pub-b093171261094c4ea9adffa01f94ee06.r2.dev/",
+    package="cmip_ref_metrics_esmvaltool.dataset_registry",
+    resource="data.txt",
+)

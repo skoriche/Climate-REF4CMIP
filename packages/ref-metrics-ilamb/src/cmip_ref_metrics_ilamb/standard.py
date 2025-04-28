0ad375dd7edb0f08bd45eb0e0b787aa451f07858
Nathan Collier <nathaniel.collier@gmail.com>
2025-04-28 15:27:29 -0400

    remove line now handled internally by ilamb3

--- packages/ref-metrics-ilamb/src/cmip_ref_metrics_ilamb/standard.py
+++ packages/ref-metrics-ilamb/src/cmip_ref_metrics_ilamb/standard.py
@@ -112,7 +112,6 @@ def _load_csv_and_merge(output_directory: Path) -> pd.DataFrame:
     df = pd.concat(
         [pd.read_csv(f, keep_default_na=False, na_values=["NaN"]) for f in output_directory.glob("*.csv")]
     ).drop_duplicates(subset=["source", "region", "analysis", "name"])
-    df["name"] = df["name"] + " [" + df["units"] + "]"
     return df
 
 

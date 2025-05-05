92ede944f9ba14174dd9c5cd9cf1d5d7cb95e5de
Jiwoo Lee <lee1043@llnl.gov>
2025-05-05 15:04:39 -0700

    update

--- packages/climate-ref-pmp/src/climate_ref_pmp/diagnostics/enso.py
+++ packages/climate-ref-pmp/src/climate_ref_pmp/diagnostics/enso.py
@@ -1,7 +1,12 @@
+from EnsoMetrics.EnsoComputeMetricsLib import ComputeCollection
+from loguru import logger
+
 from climate_ref_core.datasets import FacetFilter, SourceDatasetType
 from climate_ref_core.diagnostics import (
     CommandLineDiagnostic,
     DataRequirement,
+    ExecutionDefinition,
+    ExecutionResult,
 )
 
 
@@ -14,6 +19,7 @@ class ENSO(CommandLineDiagnostic):
         self.name = metrics_collection
         self.slug = metrics_collection.lower()
         self.metrics_collection = metrics_collection
+        self.parameter_file = "pmp_param_enso.py"
 
         def _get_data_requirements(
             metrics_collection: str,
@@ -61,3 +67,80 @@ class ENSO(CommandLineDiagnostic):
                     group_by=("source_id", "experiment_id", "variant_label", "member_id"),
                 ),
             )
+
+        self.data_requirements = _get_data_requirements(metrics_collection)
+
+    def run(self, definition: ExecutionDefinition) -> ExecutionResult:
+        """
+        Run the diagnostic on the given configuration.
+
+        Parameters
+        ----------
+        definition : ExecutionDefinition
+            The configuration to run the diagnostic on.
+
+        Returns
+        -------
+        :
+            The result of running the diagnostic.
+        """
+        # Get the input datasets information for the model
+
+        input_datasets = definition.datasets[SourceDatasetType.CMIP6]
+        source_id = input_datasets["source_id"].unique()[0]
+        experiment_id = input_datasets["experiment_id"].unique()[0]
+        member_id = input_datasets["member_id"].unique()[0]
+        variable_ids = input_datasets["variable_id"].unique()
+        mod_run = f"{source_id}_{member_id}"
+
+        dict_mod = {}
+
+        for variable in variable_ids:
+            dict_mod[mod_run][variable] = {
+                "path + filename": list_files,
+                "varname": variable,
+                "path + filename_area": list_areacell,
+                "areaname": list_name_area,
+                "path + filename_landmask": list_landmask,
+                "landmaskname": list_name_land,
+            }
+
+        # Get the input datasets information for the observations
+
+        reference_dataset = definition.datasets[SourceDatasetType.obs4MIPs]
+        reference_dataset_names = reference_dataset["source_id"].unique()
+
+        dict_obs = {}
+
+        for obs_name in reference_dataset_names:
+            for variable in variable_ids:
+                dict_obs[obs_name][variable] = {
+                    "path + filename": list_files,
+                    "varname": variable,
+                    "path + filename_area": list_areacell,
+                    "areaname": list_name_area,
+                    "path + filename_landmask": list_landmask,
+                    "landmaskname": list_name_land,
+                }
+
+        # Create input directory
+        dictDatasets = {"model": dict_mod, "observations": dict_obs}
+
+        # Computes the metric collection
+        # https://github.com/CLIVAR-PRP/ENSO_metrics/blob/d50c2613354564a155e0fe0f637eb448dfd7c479/lib/EnsoComputeMetricsLib.py#L59
+        logger.debug("\n### Compute the metric collection ###\n")
+
+        mc_name = self.metrics_collection
+
+        dict_metric = {}
+        dict_dive = {}
+
+        dict_metric, dict_dive = ComputeCollection(
+            mc_name,
+            dictDatasets,
+            mod_run,
+            netcdf=True,
+            netcdf_name=f"{mc_name}_{mod_run}_{experiment_id}",
+            debug=True,
+            obs_interpreter=True,
+        )

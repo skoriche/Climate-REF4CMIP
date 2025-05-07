76c7b977637e496ad6d61bf4c0155ec860a30dbd
Jiwoo Lee <lee1043@llnl.gov>
2025-05-06 17:59:39 -0700

    update

--- packages/climate-ref-pmp/src/climate_ref_pmp/diagnostics/enso.py
+++ packages/climate-ref-pmp/src/climate_ref_pmp/diagnostics/enso.py
@@ -1,8 +1,9 @@
-from EnsoMetrics.EnsoComputeMetricsLib import ComputeCollection  # How can I import this?
-from pcmdi_metrics.enso.lib import metrics_to_json
-from pcmdi_metrics import resources
+from EnsoMetrics.EnsoCollectionsLib import defCollection  # How can I import this?
+from EnsoMetrics.EnsoComputeMetricsLib import ComputeCollection
 
 from loguru import logger
+from pcmdi_metrics import resources
+from pcmdi_metrics.enso.lib import metrics_to_json
 
 from climate_ref_core.datasets import FacetFilter, SourceDatasetType
 from climate_ref_core.diagnostics import (
@@ -11,6 +12,9 @@ from climate_ref_core.diagnostics import (
     ExecutionDefinition,
     ExecutionResult,
 )
+
+import os
+import json
 
 
 class ENSO(CommandLineDiagnostic):
@@ -101,6 +105,7 @@ class ENSO(CommandLineDiagnostic):
 
         dict_mod = {}
 
+        # TO DO: Get the path to the files per variable
         for variable in variable_ids:
             dict_mod[mod_run][variable] = {
                 "path + filename": list_files,
@@ -119,6 +124,7 @@ class ENSO(CommandLineDiagnostic):
 
         dict_obs = {}
 
+        # TO DO: Get the path to the files per variable and per source
         for obs_name in reference_dataset_names:
             for variable in variable_ids:
                 dict_obs[obs_name][variable] = {
@@ -137,25 +143,28 @@ class ENSO(CommandLineDiagnostic):
         # Computes the metric collection
         # ------------------------------
         # https://github.com/CLIVAR-PRP/ENSO_metrics/blob/d50c2613354564a155e0fe0f637eb448dfd7c479/lib/EnsoComputeMetricsLib.py#L59
-        
+
         logger.debug("\n### PMP ENSO: Compute the metric collection ###\n")
 
         dict_metric = {}
         dict_dive = {}
+        
+        pattern = f"{mc_name}_{mod_run}_{experiment_id}"
 
         dict_metric, dict_dive = ComputeCollection(
             mc_name,
             dictDatasets,
             mod_run,
             netcdf=True,
-            netcdf_name=f"{mc_name}_{mod_run}_{experiment_id}",
+            netcdf_name=pattern,
             debug=True,
             obs_interpreter=True,
         )
-        
+
         egg_pth = resources.resource_path()
         output_directory_path = str(definition.output_directory)
-        json_name = f"{mc_name}_{mod_run}_{experiment_id}"
+        mod = self.source_id
+        run = self.member_id
 
         # OUTPUT METRICS TO JSON FILE (per simulation)
         metrics_to_json(
@@ -165,7 +174,110 @@ class ENSO(CommandLineDiagnostic):
             dict_dive,
             egg_pth,
             output_directory_path,
-            json_name,
-            mod=self.source_id,
-            run=self.member_id,
+            pattern,
+            mod=mod,
+            run=run,
         )
+        
+        # Plot
+        with open(os.path.join(output_directory_path, f"{pattern}.json")) as ff:
+            data_json = json.load(ff)["RESULTS"]["model"][mod][run]
+
+        _plot_enso(
+            mc_name,
+            mod,
+            run,
+            experiment_id,
+            os.path.join(output_directory_path, f"{pattern}.nc"),
+            output_directory_path,
+            data_json
+        )
+
+
+def _plot_enso(mc_name, mod, run, exp, path_in_nc, path_out, data_json):
+
+    metrics = sorted(
+        defCollection(mc_name)["metrics_list"].keys(), key=lambda v: v.upper()
+    )
+    print("metrics:", metrics)
+    
+    pattern = "_".join([mc_name, mod, run])
+
+    for met in metrics:
+        print("met:", met)
+        # get NetCDF file name
+        filename_nc = os.path.join(
+            path_in_nc, pattern + "_" + met + ".nc"
+        )
+        print("filename_nc:", filename_nc)
+        if os.path.exists(filename_nc):
+            # get diagnostic values for the given model and observations
+            if mc_name == "ENSO_tel" and "Map" in met:
+                dict_dia = data_json["value"][met + "Corr"]["diagnostic"]
+                diagnostic_values = dict(
+                    (key1, None) for key1 in dict_dia.keys()
+                )
+                diagnostic_units = ""
+            else:
+                dict_dia = data_json["value"][met]["diagnostic"]
+                diagnostic_values = dict(
+                    (key1, dict_dia[key1]["value"]) for key1 in dict_dia.keys()
+                )
+                diagnostic_units = data_json["metadata"]["metrics"][met][
+                    "diagnostic"
+                ]["units"]
+            # get metric values computed with the given model and observations
+            if mc_name == "ENSO_tel" and "Map" in met:
+                list1, list2 = [met + "Corr", met + "Rmse"], [
+                    "diagnostic",
+                    "metric",
+                ]
+                dict_met = data_json["value"]
+                metric_values = dict(
+                    (
+                        key1,
+                        {
+                            mod: [
+                                dict_met[su][ty][key1]["value"]
+                                for su, ty in zip(list1, list2)
+                            ]
+                        },
+                    )
+                    for key1 in dict_met[list1[0]]["metric"].keys()
+                )
+                metric_units = [
+                    data_json["metadata"]["metrics"][su]["metric"]["units"]
+                    for su in list1
+                ]
+            else:
+                dict_met = data_json["value"][met]["metric"]
+                metric_values = dict(
+                    (key1, {mod: dict_met[key1]["value"]})
+                    for key1 in dict_met.keys()
+                )
+                metric_units = data_json["metadata"]["metrics"][met]["metric"][
+                    "units"
+                ]
+            # figure name
+            figure_name = "_".join([mip, exp, mc_name, mod, run, met])
+            print("figure_name:", figure_name)
+
+            main_plotter(
+                mc_name,
+                met,
+                mod,
+                exp,
+                filename_nc,
+                diagnostic_values,
+                diagnostic_units,
+                metric_values,
+                metric_units,
+                member=run,
+                path_png=path_out,
+                name_png=figure_name,
+            )
+
+            print("figure plotting done")
+
+        else:
+            print("file not found:", filename_nc)

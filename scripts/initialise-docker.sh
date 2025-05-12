07aa0db5bb9fb4edd8d68d93882ade6ca5f873ab
Jared Lewis <jared@jared.kiwi.nz>
2025-05-12 21:09:45 +1000

    feat: Use a single container with different commands

---
+++ scripts/initialise-docker.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Script to initialise the docker-compose stack
+# This should be run to set up the docker compose stack
+
+# Build the climate-ref container locally
+docker-compose build
+
+# Run any migrations
+docker-compose run --rm climate-ref-worker config list
+
+# Initialise the conda containers
+docker-compose run --rm climate-ref-pmp providers create-env --provider pmp
+docker-compose run --rm climate-ref-esmvaltool providers create-env --provider esmvaltool

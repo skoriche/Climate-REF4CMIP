af2b6b43e65c7f6ca82389b35cf6a0a86536013e
Bouwe Andela <b.andela@esciencecenter.nl>
2025-05-22 10:29:37 +0200

    Use obs4MIPs sample data to populate obs4MIPs catalog

--- conftest.py
+++ conftest.py
@@ -118,7 +118,7 @@ def cmip6_data_catalog(sample_data_dir) -> pd.DataFrame:
 @pytest.fixture(scope="session")
 def obs4mips_data_catalog(sample_data_dir) -> pd.DataFrame:
     adapter = Obs4MIPsDatasetAdapter()
-    return adapter.find_local_datasets(sample_data_dir / "obs4REF")
+    return adapter.find_local_datasets(sample_data_dir / "obs4MIPs")
 
 
 @pytest.fixture(scope="session")

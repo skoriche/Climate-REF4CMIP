9375ad8b7051f6505349cff847db19509e21446f
Jared Lewis <jared@jared.kiwi.nz>
2025-05-26 14:45:01 +1000

    feat: Exclude all but the latest version

--- packages/climate-ref/tests/unit/datasets/test_cmip6.py
+++ packages/climate-ref/tests/unit/datasets/test_cmip6.py
@@ -52,6 +52,37 @@ class TestCMIP6Adapter:
         # The order of the rows may be flakey due to sqlite ordering and the created time resolution
         catalog_regression(df.sort_values(["instance_id", "start_time"]), basename="cmip6_catalog_db")
 
+    def test_load_catalog_multiple_versions(self, config, db_seeded, catalog_regression, sample_data_dir):
+        adapter = CMIP6DatasetAdapter()
+        data_catalog = adapter.load_catalog(db_seeded)
+        target_ds = "CMIP6.CMIP.CSIRO.ACCESS-ESM1-5.historical.r1i1p1f1.Amon.tas.gn.v20191115"
+        target_metadata = data_catalog[data_catalog["instance_id"] == target_ds]
+
+        # Make an old version
+        target_metadata.version = "v20000101"
+        target_metadata.instance_id = target_ds.replace("v20191115", "v20000101")
+        with db_seeded.session.begin():
+            adapter.register_dataset(config, db_seeded, target_metadata)
+
+        # An older version should not be in the catalog
+        pd.testing.assert_frame_equal(
+            data_catalog.sort_values(["instance_id", "start_time"]),
+            adapter.load_catalog(db_seeded).sort_values(["instance_id", "start_time"]),
+        )
+
+        # Make a new version
+        target_metadata.version = "v20230101"
+        new_instance_id = target_ds.replace("v20191115", "v20230101")
+        target_metadata.instance_id = new_instance_id
+        with db_seeded.session.begin():
+            adapter.register_dataset(config, db_seeded, target_metadata)
+
+        # The new version should be in the catalog
+        latest_data_catalog = adapter.load_catalog(db_seeded)
+        latest_instance_ids = latest_data_catalog.instance_id.unique().tolist()
+        assert target_ds not in latest_instance_ids
+        assert new_instance_id in latest_instance_ids
+
     def test_round_trip(self, db_seeded, sample_data_dir):
         # Indexes and ordering may be different
         adapter = CMIP6DatasetAdapter()
